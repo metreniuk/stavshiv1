3fd5fdbbeeb0f1b7d04b389b4cd4ba9e01982428
administrator <admin@administrators-MacBook-Air.local>
2016-08-30 21:05:57 -0500

    s

--- index.js
+++ index.js
@@ -6,14 +6,17 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
    
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static(__dirname + "/public"));
 
 app.get('/', function(request, response) {
-  response.send('hello world!');
+  response.send('hello world');
+  console.log('hello');
 });
 
-http.listen(8080, function(){
-  console.log('listening on *:8080');
+http.listen(app.get('port'), function(){
+  console.log('listening on :' + app.get('port'));
 });
 
 
