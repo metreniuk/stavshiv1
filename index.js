0328a897cec74fe56b336db6aacc4a40ef067d50
administrator <admin@administrators-MacBook-Air.local>
2016-08-30 19:27:17 -0500

    try

--- index.js
+++ index.js
@@ -8,10 +8,6 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + "/public"));
 
-app.get('/', function(request, response) {
-    response.render('index');
-})
-
 
 http.listen(8080, function(){
   console.log('listening on *:8080');
