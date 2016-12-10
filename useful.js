da7a67fced744f6dc3b5a815304cf06eb319d792
metreniuk <metreniuk@gmail.com>
2016-12-10 12:02:20 +0200

    all

--- useful.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-		arrayRotate: function (arr, count) {
-	  		count -= arr.length * Math.floor(count / arr.length);
-	  		arr.push.apply(arr, arr.splice(0, count));
-	  		return arr;
-		}
-}
